5ee8e34a942be533a3868d05dd1b0a83baae973b
Isaias Castillo <isaias.castillo@utp.ac.pa>
2025-08-25 20:53:56 -0500

    Add query parameter for selected leader in obtenerInforme route

--- backend/routes/informes/obtenerDatosInformes.js
+++ backend/routes/informes/obtenerDatosInformes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const db = require('../../db/connection');
 
 router.get("/obtenerInforme", async (req, res) => {
+    const idLiderSelected = req.query.id;
     try{
-        const result = await db.query("SELECT * FROM obtener_informes_desde(NULL, NULL)", 
+        const result = await db.query("SELECT * FROM obtener_informes_desde(NULL, NULL)",
+            [idLiderSelected] 
         );
         if (result.rows.length === 0) {
             return res.status(404).json({ message: "No se encontraron informes" });
