60b69d704dd304ce08d21ae00ff9fa2ab3ef4df2
Isaias Castillo <isaias.castillo@utp.ac.pa>
2025-08-28 12:41:20 -0500

    Refactor obtenerInformePorlider route to handle case when idLiderSelected is 0 and return appropriate response; update index.html to change option value for filtering

--- backend/routes/informes/obtenerDatosInformes.js
+++ backend/routes/informes/obtenerDatosInformes.js
@@ -20,6 +20,22 @@ router.get("/obtenerInforme", async (req, res) => {
 
 router.get("/obtenerInformePorlider", async (req, res) => {
     const idLiderSelected = req.query.id;
+    console.log("ID Líder recibido en el backend:", idLiderSelected);
+    if (idLiderSelected == 0){
+        try{
+        const result = await db.query("SELECT * FROM obtener_informes_ultima_fecha(NULL, NULL, NULL)");
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: "No se encontraron informes" });
+        }
+        return res.json({
+            message: "Informes obtenidos exitosamente",
+            result: result.rows
+        });
+    }catch (error) {
+        console.error("Error al obtener datos de informes:", error);
+        res.status(500).json({ error: "Error al obtener datos de informes" });
+    }
+    }
     try{
         const result = await db.query("SELECT * FROM obtener_informes_ultima_fecha(NULL, NULL, $1)",
             [idLiderSelected]
